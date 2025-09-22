87de2e79f1990e698fe9a56bbc92a3c58ea5d13d
Abdul Hadi <abdulhadizaeem7@gmail.com>
2025-09-22 17:25:19 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import these
 import HomePage from "./pages/HomePage";
 import ConversionPage from "./pages/ConversionPage";
 import CompressPage from "./pages/CompressionPage";
 import MergePage from "./pages/MergingPage";
 import NotFound from "./pages/NotFound";
 
+// Create QueryClient instance
 const queryClient = new QueryClient();
+
 const App = () => (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/compress" element={<CompressPage />} />
-          <Route path="/merge" element={<MergePage />} />
-          <Route path="/:type" element={<ConversionPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+  <QueryClientProvider client={queryClient}> {/* Wrap with QueryClientProvider */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/compress" element={<CompressPage />} />
+        <Route path="/merge" element={<MergePage />} />
+        <Route path="/:type" element={<ConversionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 export default App;
